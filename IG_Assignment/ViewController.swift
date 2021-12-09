d75b00cb410bb4a94d069872729c15c4f74ce991
Sakshi Jain <sakshi.jain.041@gmail.com>
2021-12-09 22:48:16 +0530

    Initial commit

---
+++ IG_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IG_Assignment
+//
+//  Created by Sakshi Jain on 09/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
