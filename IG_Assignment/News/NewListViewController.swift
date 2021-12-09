afcd5178d143402169080921ab523ba1a510e4a8
Sakshi Jain <sakshi.jain.041@gmail.com>
2021-12-10 00:35:12 +0530

    - Integrated Swift Lint - Added NewsListViewController - Integrated pods

---
+++ IG_Assignment/News/NewListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NewListViewController.swift
+//  IG_Assignment
+//
+//  Created by Sakshi Jain on 09/12/21.
+//
+
+import Foundation
+
+class NewListViewController {
+    
+}
---
+++ IG_Assignment/News/NewsListViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  NewsListViewModel.swift
+//  IG_Assignment
+//
+//  Created by Sakshi Jain on 09/12/21.
+//
+
+protocol NewsListViewModelDelegate: AnyObject {
+}
+
+import Foundation
+
+class NewsListViewModel {    
+    init(viewModelDelegate: NewsListViewModelDelegate ) {
+
+    }
+    
+    func fetchLatestNews() {
+    }
+}
